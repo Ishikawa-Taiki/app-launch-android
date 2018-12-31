5154a90c7709fae4df64d05b25caf72d497572e2
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-01-01 03:51:16 +0900

    [WIP]データ側の構造を変更 (#2)

---
+++ app-launch-android/app/src/main/java/com/example/taiki/AppLinkInfo.kt
@@ -0,0 +1,31 @@
+package com.example.taiki
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+
+class AppLinkInfo(pm: PackageManager) {
+    val pm = pm
+    var link = mutableMapOf<String, PackageInfo>()
+
+    public fun addPackageInfo(packageName: String) {
+        var intent = getApp(packageName)
+        var packageInfo = PackageInfo(intent != null, intent ?: getStore(packageName))
+        link.plusAssign(packageName to packageInfo)
+    }
+
+    public fun getPackageInfo(packageName: String): PackageInfo? {
+        return link[packageName]
+    }
+
+    private fun getApp(packageName: String): Intent? {
+        var intent = pm.getLaunchIntentForPackage(packageName)
+        return intent
+    }
+
+    private fun getStore(packageName: String): Intent {
+        var intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse("market://details?id=$packageName"))
+        return intent
+    }
+}
