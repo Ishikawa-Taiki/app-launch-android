2a0a7f5fb8e788f69871bf86ee53e208f55581b5
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-01-13 01:51:19 +0900

    [WIP]アプリケーションのアイコンを表示させる (#5)
    アプリケーションのアイコンを表示させる

--- app-launch-android/app/src/main/java/com/example/taiki/ListViewHolder.kt
+++ app-launch-android/app/src/main/java/com/example/taiki/ListViewHolder.kt
@@ -2,9 +2,11 @@ package com.example.taiki
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView
 import android.widget.TextView;
 
 class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     public var titleView = itemView.findViewById(R.id.title) as TextView;
     public var detailView = itemView.findViewById(R.id.detail) as TextView;
+    public var iconView = itemView.findViewById(R.id.icon) as ImageView
 }
