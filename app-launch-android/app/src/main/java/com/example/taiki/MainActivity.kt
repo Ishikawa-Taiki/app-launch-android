55f8b905688a68fdbee383ac89d0ac36fbfc29fc
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-01-13 19:28:30 +0900

    適当に行ったり来たりする画面遷移を実装

--- app-launch-android/app/src/main/java/com/example/taiki/MainActivity.kt
+++ app-launch-android/app/src/main/java/com/example/taiki/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Button
 import com.example.taiki.model.ApplicationData
 import com.example.taiki.model.DataModel
 import com.example.taiki.viewmodel.ListViewAdapter
@@ -12,18 +14,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         DataModel.init(applicationContext)
+        setMainScreenMode()
+    }
 
-        val rv = findViewById(R.id.listView) as RecyclerView
+    private fun setMainScreenMode() {
+        setContentView(R.layout.activity_main)
+
         val adapter = ListViewAdapter()
-
-        val llm = LinearLayoutManager(this)
-
+        val rv = findViewById(R.id.listView) as RecyclerView
         rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+        adapter.setOnItemClickListener(object : ListViewAdapter.onItemClickListener {
+            override fun onItemClick(data: ApplicationData) {
+//                data.packageName?.let {
+//                    val intent = DataModel.getAppIntent(it) ?: DataModel.getStoreIntent(it)
+//                    startActivity(intent)
+//                }
+                setSubScreenMode()
+            }
+        })
+    }
 
-        rv.layoutManager = llm
+    private fun setSubScreenMode() {
+        setContentView(R.layout.activity_sub)
 
+        val adapter = ListViewAdapter()
+        val rv = findViewById(R.id.listView) as RecyclerView
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
         adapter.setOnItemClickListener(object : ListViewAdapter.onItemClickListener {
             override fun onItemClick(data: ApplicationData) {
@@ -31,6 +51,13 @@ class MainActivity : AppCompatActivity() {
                     val intent = DataModel.getAppIntent(it) ?: DataModel.getStoreIntent(it)
                     startActivity(intent)
                 }
+            }
+        })
+
+        val returnButton = findViewById<Button>(R.id.return_button)
+        returnButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                setMainScreenMode()
             }
         })
     }
