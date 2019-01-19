d687e1de705d63864fcc8c506f722d9bace35bc0
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-01-19 14:51:58 +0900

    古い構造データを削除

--- app-launch-android/app/src/main/java/com/example/taiki/model/ApplicationData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.taiki.model
-
-class ApplicationData(appName: String, packageName: String, tags: Array<String> = emptyArray()) {
-    val appName = appName
-    val packageName = packageName
-    val tags = tags
-}
--- app-launch-android/app/src/main/java/com/example/taiki/model/InformationData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.taiki.model
-
-class InformationData(memo: String) {
-    var memo: String = memo
-}
