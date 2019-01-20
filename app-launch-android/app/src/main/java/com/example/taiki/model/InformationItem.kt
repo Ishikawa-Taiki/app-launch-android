efb3295167cc7e663f515a2201e15298d5033ad6
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-01-20 12:48:23 +0900

    文字列バグを修正 (#17)

--- app-launch-android/app/src/main/java/com/example/taiki/model/InformationItem.kt
+++ app-launch-android/app/src/main/java/com/example/taiki/model/InformationItem.kt
@@ -1,6 +1,6 @@
 package com.example.taiki.model
 
 class InformationItem(text: String, linkURL: String? = null): Item {
-    val text = text + linkURL
+    val text = text + (linkURL ?: "")
     val linkURL = linkURL
 }
