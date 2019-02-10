52250a10705c386235af031646fc3543a6f3c69c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2019-02-10 16:09:53 +0900

    最低バージョンを修正

--- app-launch-android/app/src/main/java/com/example/taiki/MainActivity.kt
+++ app-launch-android/app/src/main/java/com/example/taiki/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.taiki
 
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
