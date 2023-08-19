f293798bc4b4311b9fc56afe006b0b5fc851ace0
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-19 23:04:52 +0900

    WebAPI呼び出し処理を追加 (#31)
    * WebAPI呼び出し処理を追加
    
    * import調整
    
    * 種別データはユニオンに変更

---
+++ my_app_launcher/src/api/fetch-android-applications.ts
@@ -0,0 +1,11 @@
+import { requestWebAPI } from "./WebAPI";
+
+export type AndroidApplication = {
+	shortName: string,
+	longName: string,
+	packageName: string
+}
+
+export async function fetchAndroidApplications() {
+	return requestWebAPI<AndroidApplication[]>("exec?file=AndroidApplications&sheet=shopping");
+}
---
+++ my_app_launcher/src/api/fetch-shopping-services.ts
@@ -0,0 +1,17 @@
+import { requestWebAPI } from "./WebAPI";
+
+export type DataType = "group" // 子供をまとめるグループに使用する	ただの文字情報なのでなんでもOK
+	| "title-text"	//見出しとして表示するテキストアイテムに使用する	ただの文字情報なのでなんでもOK
+	| "text" // 表示するだけのテキストアイテムに使用する	ただの文字情報なのでなんでもOK
+	| "application" // アプリケーションに使用する	AndroidApplications の shortNameを間違いなく指定すること
+	| "link" // URLのリンクアイテムに使用する	ジャンプ可能なURLを間違いなく指定すること
+
+export type ShoppingService = {
+	data: string,
+	parentName: string,
+	type: DataType
+}
+
+export async function fetchShoppingServices() {
+	return requestWebAPI<ShoppingService[]>("exec?file=Services&sheet=shopping");
+}
