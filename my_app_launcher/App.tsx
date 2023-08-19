46da76a88857269f116134c42bc150c69ab0a061
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-20 00:35:49 +0900

    TabとStackのネスト関係を調整

--- my_app_launcher/App.tsx
+++ my_app_launcher/App.tsx
@@ -12,27 +12,27 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Feed"
-        screenOptions={{
-          tabBarActiveTintColor: '#e91e63',
-          tabBarLabelStyle: { fontSize: 12 },
-          tabBarStyle: { backgroundColor: 'powderblue' },
-        }}>
-        <Tab.Screen name="ShoppingStack" component={ShoppingStack}
-          options={{ tabBarLabel: 'お買い物' }} />
-        <Tab.Screen name="ApplicationList" component={ApplicationList}
-          options={{ tabBarLabel: 'アプリ一覧' }} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Tabs" component={Tabs} />
+        <Stack.Screen name="ShoppingDetail" component={ShoppingDetail} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function ShoppingStack() {
+function Tabs() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Shopping" component={Shopping} />
-      <Stack.Screen name="ShoppingDetail" component={ShoppingDetail}/>
-    </Stack.Navigator>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+        tabBarLabelStyle: { fontSize: 12 },
+        tabBarStyle: { backgroundColor: 'powderblue' },
+      }}>
+      <Tab.Screen name="Shopping" component={Shopping}
+        options={{ tabBarLabel: 'お買い物' }} />
+      <Tab.Screen name="ApplicationList" component={ApplicationList}
+        options={{ tabBarLabel: 'アプリ一覧' }} />
+    </Tab.Navigator>
   )
 }
