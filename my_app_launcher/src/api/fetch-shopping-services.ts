c61a7b0d0bcac4748d5bf0ad146cbb755d58fc6b
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-19 23:02:49 +0900

    種別データはユニオンに変更

--- my_app_launcher/src/api/fetch-shopping-services.ts
+++ my_app_launcher/src/api/fetch-shopping-services.ts
@@ -1,9 +1,15 @@
 import { requestWebAPI } from "./WebAPI";
 
+export type DataType = "group" // 子供をまとめるグループに使用する	ただの文字情報なのでなんでもOK
+	| "title-text"	//見出しとして表示するテキストアイテムに使用する	ただの文字情報なのでなんでもOK
+	| "text" // 表示するだけのテキストアイテムに使用する	ただの文字情報なのでなんでもOK
+	| "application" // アプリケーションに使用する	AndroidApplications の shortNameを間違いなく指定すること
+	| "link" // URLのリンクアイテムに使用する	ジャンプ可能なURLを間違いなく指定すること
+
 export type ShoppingService = {
 	data: string,
 	parentName: string,
-	type: string
+	type: DataType
 }
 
 export async function fetchShoppingServices() {
