e08528ac7cedfa9cc2555f958c8c3cca06bb7954
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-20 00:36:18 +0900

    グループ用アイテムのUIを作成

---
+++ my_app_launcher/src/const.ts
@@ -0,0 +1,20 @@
+export const ViewSpec = {
+	FontSize: {
+		listItem: 24,
+	},
+	ImageSize: {
+		listIcon: 48,
+	},
+	Color: {
+		listBackground: '#ffffff',
+		todo: '#ff0000',
+	},
+	Margin: {
+		listItem: 8,
+		imageText: 16,
+	},
+	BorderStyle: {
+		listBorderColor: '#E1E6E6',
+		listBorderWidth: 1,
+	}
+}
