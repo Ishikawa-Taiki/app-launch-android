6b02e11e3bd27dbaf5c7a48723da2cdc85d4b4c7
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-20 18:56:42 +0900

    applications用のセレクタを追加

--- my_app_launcher/src/features/shopping/slice.ts
+++ my_app_launcher/src/features/shopping/slice.ts
@@ -61,6 +61,9 @@ export const {} = counterSlice.actions;
 export const selectServices = (state: RootState) => state.shopping.data.services;
 export const selectServicesByParentName = (state: RootState, parentName: string) =>
   state.shopping.data.services.filter((service) => service.parentName === parentName);
+export const selectApplications = (state: RootState) => state.shopping.data.applications;
+export const selectApplicationsByShortName = (state: RootState, shortName: string) =>
+  state.shopping.data.applications.filter((application) => application.shortName === shortName);
 
 const SHOPPING_STORAGE_KEY = 'shopping-services';
 const APPLICATION_STORAGE_KEY = 'android-applications';
