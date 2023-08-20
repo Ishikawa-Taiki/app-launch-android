7dc9256df3231cda87ba78387280c779d3b8f2e4
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-20 14:52:26 +0900

    画面関連コンポーネントを機能フォルダへ移動

--- my_app_launcher/src/features/shopping/pages/ShoppingDetail.tsx
+++ my_app_launcher/src/features/shopping/pages/ShoppingDetail.tsx
@@ -1,6 +1,6 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
-import ShoppingServiceList from '../template/ShoppingServiceList';
-import { ShoppingService } from '../api/fetch-shopping-services';
+import ShoppingServiceList from '../../../template/ShoppingServiceList';
+import { ShoppingService } from '../../../api/fetch-shopping-services';
 
 export default function ShoppingDetail(props: { route: { params: any[] }; navigation: any }) {
   return <ShoppingServiceList filter={props.route.params.filter} navigation={props.navigation} />;
