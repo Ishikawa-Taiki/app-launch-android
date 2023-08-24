add9e89fabb181f00d5431cdbe64d47fc0644627
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-24 23:20:40 +0900

    検索ボックス追加

--- my_app_launcher/src/features/installed/pages/ApplicationList.tsx
+++ my_app_launcher/src/features/installed/pages/ApplicationList.tsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, TextInput, View } from 'react-native';
 
 import { Package, installedPackages } from '../../../../modules/expo-installed-application-view';
 import { ApplicationCommon } from '../../../common/components/ApplicationCommon';
+import { ViewSpec } from '../../../common/const';
 
 export default function ApplicationList() {
   const [appPackages, setAppPackages] = useState([] as Package[]);
+  const [searchText, onChangeSearchText] = useState('');
   useEffect(() => {
     installedPackages().then((packages) => {
       setAppPackages(packages.sort((a, b) => a.loadLabel.localeCompare(b.loadLabel)));
     });
   }, []);
 
+  const listData = searchText
+    ? appPackages.filter((appPackage) => appPackage.loadLabel.includes(searchText))
+    : appPackages;
+
   return (
     <View style={styles.container}>
-      <FlatList data={appPackages} renderItem={({ item }) => <Item appPackage={item} />} />
+      <FlatList data={listData} renderItem={({ item }) => <Item appPackage={item} />} />
+      <TextInput
+        style={styles.searchBox}
+        onChangeText={onChangeSearchText}
+        value={searchText}
+        placeholder={ViewSpec.TextDefinition.searchBoxPlaceholder}
+      />
     </View>
   );
 }
@@ -32,5 +44,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  searchBox: {
+    backgroundColor: ViewSpec.Color.disableListBackground,
+    margin: ViewSpec.Margin.listItem,
+    height: ViewSpec.ImageSize.listIcon,
+    borderWidth: ViewSpec.BorderStyle.listBorderWidth,
+    borderColor: ViewSpec.BorderStyle.listBorderColor,
   },
 });
