eb8a13e9e03627107606131cad6882f897aba875
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2023-08-27 12:50:33 +0900

    lintとprettierの調整

--- my_app_launcher/.prettierrc.js
+++ my_app_launcher/.prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-	jsxSingleQuote: true,
-	singleQuote: true,
-	trailingComma: 'all',
-	printWidth: 100,
-	endOfLine: 'lf',
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
 };
